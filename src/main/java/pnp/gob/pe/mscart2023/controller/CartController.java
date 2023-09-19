98709d37308fbc5b4dc1352289f401540f4409ae
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2023-09-19 00:01:48 -0500

    Vamos avanzando con el ms-cart del video2

---
+++ src/main/java/pnp/gob/pe/mscart2023/controller/CartController.java
@@ -0,0 +1,78 @@
+package pnp.gob.pe.mscart2023.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import pnp.gob.pe.mscart2023.model.dto.CartRequestDeleteDto;
+import pnp.gob.pe.mscart2023.model.dto.CartRequestDto;
+import pnp.gob.pe.mscart2023.model.dto.CartResponseDto;
+import pnp.gob.pe.mscart2023.service.CartService;
+
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+//@RefreshScope
+@RequiredArgsConstructor
+@RestController
+@RequestMapping(path = "v1.0")
+public class CartController {
+
+    private final CartService service;
+
+    //@Value("${configuration.texto}")
+    private String text;
+
+    private final Environment env;
+
+    @GetMapping(value = "/{customerId}", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE })
+    public ResponseEntity<CartResponseDto> findByCustomerId(@PathVariable Long customerId){
+        CartResponseDto result = service.findByCustomerId(customerId);
+        return ResponseEntity.ok(result);
+    }
+
+    @PostMapping(value = "/{customerId}/item", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE })
+    public ResponseEntity<CartResponseDto> addItem(@PathVariable Long customerId,
+                                                   @Valid @RequestBody CartRequestDto cartRequestDto) {
+        CartResponseDto result = service.addItem(customerId, cartRequestDto);
+        return ResponseEntity.ok(result);
+    }
+
+    @DeleteMapping(value = "/{customerId}/item", produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE },
+            consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_PROBLEM_JSON_VALUE })
+    public ResponseEntity<CartResponseDto> removeItem(@PathVariable Long customerId,
+                                                      @Valid @RequestBody CartRequestDeleteDto cartRequestDeleteDto) {
+        CartResponseDto result = service.removeItem(customerId, cartRequestDeleteDto);
+        return ResponseEntity.ok(result);
+    }
+
+    @GetMapping("/configuration")
+    public ResponseEntity<?> getConfiguration(@Value("${server.port}") String port){
+        Map<String, String> json = new HashMap<>();
+        json.put("texto", text);
+        json.put("puerto", port);
+
+        if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+            json.put("autor.nombre", env.getProperty("configuration.autor.nombre"));
+            json.put("autor.email", env.getProperty("configuration.autor.email"));
+        }
+
+        return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+    }
+
+}
--- src/main/java/pnp/gob/pe/mscart2023/service/CartService.java
+++ src/main/java/pnp/gob/pe/mscart2023/service/CartService.java
@@ -7,9 +7,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.transaction.annotation.Transactional;
 import pnp.gob.pe.mscart2023.configuration.error.ResourceNotFoundException;
+import pnp.gob.pe.mscart2023.model.dto.CartRequestDeleteDto;
+import pnp.gob.pe.mscart2023.model.dto.CartRequestDto;
 import pnp.gob.pe.mscart2023.model.dto.CartResponseDto;
+import pnp.gob.pe.mscart2023.model.dto.ProductResponseDto;
+import pnp.gob.pe.mscart2023.model.entity.CartEntity;
 import pnp.gob.pe.mscart2023.model.mapper.CartMapper;
 import pnp.gob.pe.mscart2023.repository.CartRepository;
+
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -20,10 +26,55 @@ public class CartService {
 	
 	private final CartMapper mapper;
 
+	private final ProductService productService;
+
 	@Transactional(readOnly = true)
 	public CartResponseDto findByCustomerId(Long customerId){
 		log.info("findByCustomerId");
 		return cartRepository.findByCustomerId(customerId)
 				.map(mapper::entityToResponse).orElseThrow(() -> new ResourceNotFoundException("Resource not found", HttpStatus.NOT_FOUND));
+	}
+
+	@Transactional
+	public CartResponseDto addItem(Long customerId, CartRequestDto cartRequestDto){
+		log.info("addItem");
+		Optional<CartEntity> cartOptional = cartRepository.findByCustomerId(customerId);
+		if (cartOptional.isEmpty()){
+			throw new ResourceNotFoundException("Resource not found", HttpStatus.NOT_FOUND);
+		}
+
+		CartEntity cartEntity = cartOptional.get();
+
+		cartRequestDto.getItems().forEach(p -> {
+			ProductResponseDto product = productService.findById(p.getProductId());
+			if (product == null){
+				throw new ResourceNotFoundException("Product not found with id " + p.getProductId(), HttpStatus.NOT_FOUND);
+			}else{
+				cartEntity.getItems().add(mapper.responseToEntity(product, p.getQuantity()));
+			}
+		});
+
+		cartRepository.save(cartEntity);
+
+		return mapper.entityToResponse(cartEntity);
+	}
+
+	@Transactional
+	public CartResponseDto removeItem(Long customerId, CartRequestDeleteDto cartRequestDeleteDto){
+		log.info("removeItem");
+		Optional<CartEntity> cartOptional = cartRepository.findByCustomerId(customerId);
+		if (cartOptional.isEmpty()){
+			throw new ResourceNotFoundException("Resource not found", HttpStatus.NOT_FOUND);
+		}
+
+		CartEntity cartEntity = cartOptional.get();
+
+		cartRequestDeleteDto.getItems().forEach(p -> {
+			cartEntity.getItems().removeIf(pt -> pt.getProductId() == p.getProductId());
+		});
+
+		cartRepository.save(cartEntity);
+
+		return mapper.entityToResponse(cartEntity);
 	}
 }
