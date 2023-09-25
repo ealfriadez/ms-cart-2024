7bfa47d324b9a5a458305f75bf91a5937fa2b343
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2023-09-25 17:58:25 -0500

    Actualizando codigo en la rama feature/codearti en origin

--- src/main/java/pnp/gob/pe/MsCart2024Application.java
+++ src/main/java/pnp/gob/pe/MsCart2024Application.java
@@ -2,11 +2,14 @@ package pnp.gob.pe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class MsCart2024Application {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MsCart2024Application.class, args);
